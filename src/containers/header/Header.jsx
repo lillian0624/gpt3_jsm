74ea62dfcb1eeb3b9021b6a297a645a6d910a784
Lillian Yang <nopeyangli@gmai.com>
2023-11-27 12:17:18 +1100

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,11 @@ import './header.css';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='hpt3__header section__padding' id="home">
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something amazing with FPT-3 OpenAI</h1>
+      </div>
+    </div>
   )
 }
 
