23b7c6bb779f3fd83b7b9c4ec35926f4fbb6064b
Lillian Yang <nopeyangli@163.com>
2023-12-01 15:01:16 +1100

    blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -13,14 +13,14 @@ const Blog = () => {
    
       <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container_groupA'>
-          <Article imgUrl={blog01} date="Dec "/>
+          <Article imgUrl={blog01} date="Dec 1, 2023 " title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
         </div>
           <div className='gpt3__blog-container_groupB'>
             
-            <Article imgUrl={blog02} date="Dec "/>
-            <Article imgUrl={blog03} date="Dec "/>
-            <Article imgUrl={blog04} date="Dec "/>
-            <Article imgUrl={blog05} date="Dec "/>
+            <Article imgUrl={blog02} date="Dec 1, 2023 " title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+            <Article imgUrl={blog03} date="Dec 1, 2023 " title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+            <Article imgUrl={blog04} date="Dec 1, 2023 " title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+            <Article imgUrl={blog05} date="Dec 1, 2023 " title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
           </div>
         </div>
     </div>
